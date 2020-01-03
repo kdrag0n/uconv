cd349c06f9de226903acc3655488454db9250463
Danny Lin <danny@kdrag0n.dev>
2020-01-02 18:45:51 -0800

    main: Pretty-print parsed units and graphs

--- uconv/main.py
+++ uconv/main.py
@@ -1,2 +1,10 @@
-def main():
-    pass
+from pprint import pprint
+
+from . import parser
+
+
+def main() -> None:
+    units, graph = parser.parse_table("units.toml")
+    pprint(units)
+    print()
+    pprint(graph)
